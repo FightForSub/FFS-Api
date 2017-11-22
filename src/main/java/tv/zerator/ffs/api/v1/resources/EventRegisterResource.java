628d00adcc6e1016827fffd9b44a864fe23c183a
AlexMog <alexmog@live.fr>
2017-11-22 20:04:42 +0100

    Twitch user's data update

--- src/main/java/tv/zerator/ffs/api/v1/resources/EventRegisterResource.java
+++ src/main/java/tv/zerator/ffs/api/v1/resources/EventRegisterResource.java
@@ -1,8 +1,25 @@
 package tv.zerator.ffs.api.v1.resources;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.sql.SQLException;
 import java.util.UUID;
+import java.util.logging.Level;
 
+import org.apache.commons.io.IOUtils;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.protocol.HttpClientContext;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.codehaus.jackson.annotate.JsonMethod;
+import org.codehaus.jackson.annotate.JsonAutoDetect.Visibility;
+import org.codehaus.jackson.map.DeserializationConfig;
+import org.codehaus.jackson.map.ObjectMapper;
+import org.codehaus.jackson.map.annotate.JsonSerialize;
+import org.codehaus.jackson.mrbean.MrBeanModule;
 import org.restlet.data.Status;
 import org.restlet.resource.Delete;
 import org.restlet.resource.Post;
@@ -13,19 +30,44 @@ import org.restlet.resource.ServerResource;
 import alexmog.apilib.api.validation.ValidationErrors;
 import alexmog.apilib.exceptions.BadEntityException;
 import alexmog.apilib.exceptions.ConflictException;
+import alexmog.apilib.exceptions.InternalServerError;
 import alexmog.apilib.exceptions.NotFoundException;
 import alexmog.apilib.managers.DaoManager.DaoInject;
+import tv.zerator.ffs.api.Main;
+import tv.zerator.ffs.api.dao.AccountsDao;
 import tv.zerator.ffs.api.dao.EventsDao;
 import tv.zerator.ffs.api.dao.EventsDao.UserStatus;
 import tv.zerator.ffs.api.dao.beans.AccountBean;
 import tv.zerator.ffs.api.dao.beans.AccountBean.BroadcasterType;
+import tv.zerator.ffs.api.v1.ApiV1;
+import tv.zerator.ffs.api.v1.resources.LoginResource.TwitchChannelObject;
 import tv.zerator.ffs.api.dao.beans.EventBean;
 
 public class EventRegisterResource extends ServerResource {
 	@DaoInject
+	private static AccountsDao mAccounts;
+	@DaoInject
 	private static EventsDao mEvents;
+	private static final ObjectMapper mMapper;
+	private final CloseableHttpClient mHttpClient = HttpClients.createDefault();
+	private static String mTwitchUserUrl, mTwitchClientId, mTwitchChannelUrl = null, mTwitchAccept;
 
 	private int mEventId;
+	
+	static {
+		mMapper = new ObjectMapper().enable(DeserializationConfig.Feature.ACCEPT_SINGLE_VALUE_AS_ARRAY).enableDefaultTyping(ObjectMapper.DefaultTyping.NON_FINAL)
+		        .setSerializationInclusion(JsonSerialize.Inclusion.NON_NULL).setVisibility(JsonMethod.FIELD, Visibility.ANY);
+	    mMapper.registerModule(new MrBeanModule());
+	    mMapper.disableDefaultTyping();
+	}
+	
+	public EventRegisterResource() {
+		if (mTwitchChannelUrl != null) return;
+		mTwitchUserUrl = Main.getInstance().getConfig().getProperty("twitch.user_url");
+		mTwitchClientId = Main.getInstance().getConfig().getProperty("twitch.client_id");
+		mTwitchChannelUrl = Main.getInstance().getConfig().getProperty("twitch.channels_url");
+		mTwitchAccept = Main.getInstance().getConfig().getProperty("twitch.accept_header");
+	}
 
 	@Override
 	protected void doInit() throws ResourceException {
@@ -45,7 +87,41 @@ public class EventRegisterResource extends ServerResource {
 		if (!event.isReservedToAffiliates() && !event.isReservedToPartners()
 				|| event.isReservedToAffiliates() && acc.getBroadcasterType() == BroadcasterType.affiliate
 				|| event.isReservedToPartners() && acc.getBroadcasterType() == BroadcasterType.partner) {
-			mEvents.registerUser(mEventId, acc.getTwitchId(), UserStatus.AWAITING_FOR_ADMIN_VALIDATION, UUID.randomUUID().toString());
+			try {
+				String json;
+				int retCode;
+				HttpGet httpGet;
+				httpGet = new HttpGet(new URI(mTwitchUserUrl));
+				httpGet.addHeader("Accept", mTwitchAccept);
+				httpGet.addHeader("Client-ID", mTwitchClientId);
+				httpGet.setURI(new URI(mTwitchChannelUrl + acc.getTwitchId()));
+
+				try (CloseableHttpResponse resp = mHttpClient.execute(httpGet, HttpClientContext.create());) {
+					InputStream stream = resp.getEntity().getContent();
+					json = IOUtils.toString(stream);
+					retCode = resp.getStatusLine().getStatusCode();
+				}
+				
+				httpGet.releaseConnection();
+				
+				TwitchChannelObject channelObject = mMapper.readValue(json, TwitchChannelObject.class);
+				if (retCode != 200 || channelObject.error != null) throw new NotFoundException("Channel not found on twitch api.");
+				
+				acc.setBroadcasterType(channelObject.broadcaster_type == null || channelObject.broadcaster_type.length() == 0 ? BroadcasterType.none : BroadcasterType.valueOf(channelObject.broadcaster_type));
+				acc.setFollowers(channelObject.followers);
+				acc.setTwitchId(channelObject._id);
+				acc.setUrl(channelObject.url);
+				acc.setUsername(channelObject.display_name);
+				acc.setViews(channelObject.views);
+				acc.setLogo(channelObject.logo);
+
+				mAccounts.update(acc);
+
+				mEvents.registerUser(mEventId, acc.getTwitchId(), UserStatus.AWAITING_FOR_ADMIN_VALIDATION, UUID.randomUUID().toString());
+			} catch (URISyntaxException | IOException e) {
+				Main.LOGGER.log(Level.SEVERE, "Impossible to login", e);
+				throw new InternalServerError("Cannot verify your authentication. Try contacting an admin.");
+			}
 			return Status.SUCCESS_OK;
 		} else throw new ConflictException("You are not eligible for this event.");
 	}
