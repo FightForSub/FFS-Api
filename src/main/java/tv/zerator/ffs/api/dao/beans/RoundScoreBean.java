b620922f36e760f612a06c5a6107582d95b1fd55
Grégoire Daussin <gregoire.daussin@caudalie.com>
2018-03-12 16:38:06 +0100

    Change score type to double

--- src/main/java/tv/zerator/ffs/api/dao/beans/RoundScoreBean.java
+++ src/main/java/tv/zerator/ffs/api/dao/beans/RoundScoreBean.java
@@ -3,5 +3,6 @@ package tv.zerator.ffs.api.dao.beans;
 import lombok.Data;
 
 public @Data class RoundScoreBean {
-	private int roundId, accountId, score;
+	private int roundId, accountId;
+	private double score;
 }
