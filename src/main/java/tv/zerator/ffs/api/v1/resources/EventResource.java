8e45c04b7a39d4d3416bb4b8ccfdc80c5540dff5
vdumestre <vincentdumestre@protonmail.com>
2019-02-25 23:59:23 +0100

    Add a ranking system when an event state change to ENDED

--- src/main/java/tv/zerator/ffs/api/v1/resources/EventResource.java
+++ src/main/java/tv/zerator/ffs/api/v1/resources/EventResource.java
@@ -1,6 +1,7 @@
 package tv.zerator.ffs.api.v1.resources;
 
 import java.sql.SQLException;
+import java.util.*;
 
 import org.restlet.data.Status;
 import org.restlet.resource.Delete;
@@ -49,6 +50,11 @@ public class EventResource extends ServerResource {
 		err.checkErrors("EVENT_UPDATE_ERROR");
 		
 		EventBean bean = mEvents.getEvent(mEventId);
+
+		if (entity.status == EventBean.Status.ENDED && bean.getStatus() != EventBean.Status.ENDED)
+			rankAllUsers(mEventId);
+
+
 		if (bean == null) throw new NotFoundException("EVENT_NOT_FOUND");
 		bean.setCurrent(entity.current);
 		bean.setDescription(entity.description);
@@ -60,6 +66,9 @@ public class EventResource extends ServerResource {
 		bean.setMinimumFollowers(entity.minimum_followers);
 		bean.setRankingType(entity.ranking_type);
 		mEvents.update(bean);
+
+
+
 		return Status.SUCCESS_OK;
 	}
 	
@@ -69,12 +78,80 @@ public class EventResource extends ServerResource {
 		mEvents.delete(mEventId);
 		return Status.SUCCESS_OK;
 	}
-	
+
 	private static class UpdateEventEntity {
 		public String name, description;
 		public boolean current, reserved_to_affiliates, reserved_to_partners;
 		public int minimum_views, minimum_followers;
 		public EventBean.Status status;
 		public EventBean.RankingType ranking_type;
+	}
+
+	private void rankAllUsers(int eventId) throws SQLException {
+		EventBean event = mEvents.getEvent(mEventId);
+		List<EventsDao.RoundUserScoreBean> allScores = mEvents.getAllScores(eventId);
+
+		Set<Integer> lastUsers = new HashSet<>();
+
+		Map<Integer,Double> map = new HashMap<>();
+
+		for (EventsDao.RoundUserScoreBean rus:
+				allScores) {
+			if (rus.getScore() == 0){
+				lastUsers.add(rus.getId());
+			}
+			if (map.containsKey(rus.getId())){
+				map.put(rus.getId(),map.get(rus.getId()) + rus.getScore());
+			} else {
+				map.put(rus.getId(),rus.getScore());
+			}
+		}
+
+		for (Integer userId:
+			 lastUsers) {
+			map.remove(userId);
+		}
+
+		List<Map.Entry<Integer,Double>> list = new ArrayList<>(map.entrySet());
+		Collections.sort(list, new ScoreUserValueComparator(event.getRankingType()));
+
+		int i = 1;
+		int currentRank = 1;
+		Double currentScore = null ;
+		for (Map.Entry<Integer,Double> entry:
+				list) {
+
+			if (currentScore == null || !currentScore.equals(entry.getValue())){
+				currentRank = i;
+				currentScore = entry.getValue();
+			}
+
+			mEvents.updateUserRank(mEventId, entry.getKey(), currentRank);
+
+			i++;
+		}
+
+		for (Integer userId:
+			 lastUsers) {
+			mEvents.updateUserRank(mEventId, userId, i);
+		}
+
+	}
+
+	class ScoreUserValueComparator implements Comparator<Map.Entry<Integer, Double>> {
+		EventBean.RankingType rankingType;
+
+		ScoreUserValueComparator(EventBean.RankingType rankingType) {
+			this.rankingType = rankingType;
+		}
+
+		@Override
+		public int compare(Map.Entry<Integer, Double> e1, Map.Entry<Integer, Double> e2) {
+			if (rankingType.equals(EventBean.RankingType.SCORE_ASC)){
+				return Double.compare(e1.getValue(),e2.getValue());
+			} else {
+				return Double.compare(e2.getValue(),e1.getValue());
+			}
+		}
 	}
 }
